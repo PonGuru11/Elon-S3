423727c7a05ba9076b74a8a8cea7d96c2f79e4ad
PonGuru11 <125974004+PonGuru11@users.noreply.github.com>
2023-04-29 10:36:10 +0530

    first commit

---
+++ services/envelopesService.js
@@ -0,0 +1,41 @@
+const axios = require('axios');
+const EnvelopeDAO = require('../dao/envelopeDAO');
+
+exports.createEnvelope = async (documentUrl, email, name) => {
+const response = await axios.get(documentUrl, { responseType: 'arraybuffer' });
+const documentBase64 = Buffer.from(response.data, 'binary').toString('base64');
+
+ const payload = {
+    documents: [
+      {
+        documentBase64 ,
+        documentId: "1",
+        fileExtension: 'doc' || 'pdf',
+        name : "sampl.doc"
+      }
+    ],
+    emailSubject: 'Sign the Document',
+    recipients: {
+      signers: [
+        {
+          email,
+          name,
+          recipientId: "1"
+        }
+      ]
+    },
+    status: 'created'
+  };
+
+  // Send the request to DocuSign API
+  const envelopeResponse = await axios.post(
+    EnvelopeDAO.getEnvelopeApiUrl(),
+    payload,
+    { headers: EnvelopeDAO.getEnvelopeApiHeaders() }
+  );
+
+  return envelopeResponse.data;
+};
+
+
+
